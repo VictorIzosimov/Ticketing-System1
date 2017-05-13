b0c5bde5f0705a7291a85d76546e099971a3d274
VictorIzosimov <izosimov.victor@gmail.com>
2017-05-13 16:31:16 +0800

    Update Global.asax.cs

--- TicketingSystem/Global.asax.cs
+++ TicketingSystem/Global.asax.cs
@@ -18,7 +18,7 @@ namespace TicketingSystem
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
         
-            protected void Application_Error(object sender, EventArgs e)
+        protected void Application_Error(object sender, EventArgs e)
         {
             Exception exception = Server.GetLastError();
             Server.ClearError();
